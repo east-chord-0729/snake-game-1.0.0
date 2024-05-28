a0829ce63cf2ec2b8d2fe6675cd06ac57fa47f43
realiron00 <ic1595@kookmin.ac.kr>
2024-05-28 13:40:48 +0900

    05281340_wall

--- JC/New_src/ObjectWalls.hpp
+++ JC/New_src/ObjectWalls.hpp
@@ -1,1 +1,24 @@
 #pragma once
+#include "Object.hpp"
+
+class Wall : public Object
+{
+    public:
+        Wall(int y = 0, int x = 0)
+        {
+            this->y = y;
+            this->x = x;
+            this->icon = ICON_WALL;
+        }
+};
+
+class ImmuneWall : public Object
+{
+    public:
+        ImmuneWall(int y = 0, int x = 0)
+        {
+            this->y = y;
+            this->x = x;
+            this->icon = ICON_IWALL;
+        }
+};
